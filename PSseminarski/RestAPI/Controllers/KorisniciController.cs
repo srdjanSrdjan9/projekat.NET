7e1899ace018c8de02a23fe0a3c4553b3dfb06b0
Srdjan Mladjenovic <mladjenovic.srdjan@live.com>
2016-07-02 15:58:25 +0200

    kontroler i model

---
+++ PSseminarski/RestAPI/Controllers/KorisniciController.cs
@@ -0,0 +1,38 @@
+﻿using Model;
+using RestAPI.Models;
+using SistemskeOperacije;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestAPI.Controllers
+{
+    [RoutePrefix("api/Korisnici")]
+    public class KorisniciController : ApiController
+    {
+        [HttpPost]
+        [Route("kreiranje")]
+        public IHttpActionResult createUser([FromBody]KreiranjeKor k)
+        {
+            SOKreirajKorisnika so = new SOKreirajKorisnika();
+            Korisnik kor = new Korisnik()
+            {
+                Ime = k.Ime,
+                Prezime = k.Prezime,
+                Adresa = k.Adresa,
+                Jmbg = k.Jmbg
+            };
+            if (so.IzvrsiSo(kor))
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+         }
+    }
+}
---
+++ PSseminarski/RestAPI/Models/KreiranjeKor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestAPI.Models
+{
+    public class KreiranjeKor
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Adresa { get; set; }
+        public string Jmbg { get; set; }
+        
+        public KreiranjeKor() { }
+    }
+}
