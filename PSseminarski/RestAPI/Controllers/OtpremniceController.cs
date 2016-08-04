ef85082af9f43244c68527b460f5d16ada9377e8
Windows User <Windows User@MRKI>
2016-08-04 19:35:57 +0200

    otpremnice

---
+++ PSseminarski/RestAPI/Controllers/OtpremniceController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Model;
+using SistemskeOperacije;
+
+namespace RestAPI.Controllers
+{
+    [RoutePrefix("api/Otpremnice")]
+    public class OtpremniceController : ApiController
+    {
+        [HttpPost]
+        [Route("kreiranje")]
+        public IHttpActionResult createOtpremnica([FromBody]Otpremnica o)
+        {
+            SOKreiranjeOtpremnice sok = new SOKreiranjeOtpremnice();
+            if (sok.IzvrsiSo(o))
+            {
+                return Ok();
+            }
+            else
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
